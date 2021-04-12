bd0a9c608172856309d8f6d7f26e4b089eb2704c
Leo Liu <auleo.liu@gmail.com>
2021-04-12 10:40:55 +1000

    python conversion

--- to_sqlite.py
+++ to_sqlite.py
@@ -5,6 +5,7 @@ import csv
 import zipfile as zip
 import pandas as pd
 import requests
+import urllib
 
 db = sqlite3.connect('patentsview.db')
 website = requests.get('https://patentsview.org/download/data-download-tables').text.split()
@@ -14,5 +15,11 @@ for x in website:
         url = re.findall('http.*\.tsv\.zip',x)[0]
         fn = url.split('/')[-1].split('.')[0]
         print(url, fn)
-        df = pd.read_csv(url,delimiter="\t",quoting = csv.QUOTE_NONNUMERIC,low_memory=False)
+        try:
+            df = pd.read_csv(url,delimiter="\t",quoting = csv.QUOTE_NONNUMERIC,low_memory=False)
+        except:
+            newfn = fn+'.tsv.zip'
+            urllib.request.urlretrieve (url, newfn)
+            df = pd.read_csv(newfn,delimiter="\t",quoting = csv.QUOTE_NONNUMERIC,low_memory=False)
+            os.remove(newfn)
         df.to_sql(fn,db,index=False,if_exists='replace')
